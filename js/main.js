12b203fbad4ecaaf96bb95089cba12b665d64a52
Shi Hao <shihaotan91@gmail.com>
2016-09-15 03:12:01 +0800

    first javascript

--- js/main.js
+++ js/main.js
@@ -5,19 +5,8 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne == cardTwo)
-
-{console.log("equal");}
-
-else
-
-{console.log("not equal");}
-
-
-if (cardThree == cardFour)
-
-{console.log("equal");}
-
-else
-
-{console.log("not equal");}
+if (cardOne === cardTwo) {
+	alert('You found a match!');	
+} else {
+  alert('Sorry, try again.');
+}
