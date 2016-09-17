62a720624354a683d4cf3cb5ca9090d5240bb891
Shi Hao <shihaotan91@gmail.com>
2016-09-18 04:58:53 +0800

    edited JS

--- js/main.js
+++ js/main.js
@@ -1,30 +1,48 @@
-console.log("JS file is connected to HTML! Woo!")
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var board = document.getElementById('game-board');
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var createboard = function() {
+	for (var i = 0; i < cards.length; i ++) {
+		 var cardElement = document.createElement('div');
+		 cardElement.className = 'card';
+		}
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards)
 
-if (cardOne === cardTwo) {
-	alert('You found a match!');	
-} else {
-  alert('Sorry, try again.');
+		 board.appendChild(cardElement);
+         board.appendChild(cardElement);
+     }
+
+	};
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src ='https://upload.wikimedia.org/wikipedia/commons/9/9b/Poker-sm-212-Ks.png'>"; // king
+	} else {
+		this.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/1/1e/Poker-sm-243-Qc.png'>"; //queen
+	}
+
+  if (cardsInPlay.length === 2) {
+
+    isMatch(cardsInPlay);
+
+    cardsInPlay = [];
+
+  }
+
+
+function isMatch(cards) {
+  // alert winning message
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
 }
 
-document.getElementsByClassName('board').setAttribute('id', 'game-board');
-
-var gameboard = document.getElementsById('game-board');
-
-var createboard = function(); {
-	for (var i = 0; i < card.length; i ++) {
-
-var cardElement = document.createElement('div');
-
-cardElement.className = 'card';
-
-board.appendChild(cardElement);
-
-} 
-
-}
-
+createBoard();
